5dc6f82b56bf6900503786ac563176725ebf8924
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-12-27 16:52:59 -0500

    Inluir cambio de pasos en el formulario
    [+] Diferentes de formulario implementado
    [+] Incluir el logo de naves en el icono de la pagina web

--- src/app/viajes-detalle/viajes-detalle.component.ts
+++ src/app/viajes-detalle/viajes-detalle.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'viajes-detalle',
@@ -6,10 +6,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./viajes-detalle.component.scss']
 })
 export class ViajesDetalleComponent implements OnInit {
+  @ViewChild('formContent', {static: true}) formContent: ElementRef;
+  step: number;
 
-  constructor() { }
+  constructor() {
+    this.step = 1;
+  }
 
   ngOnInit(): void {
+  }
+
+  verPrimerStep(){
+    const content = this.formContent.nativeElement;
+    content.style.marginLeft = '0%';
+    this.step = 1;
+  }
+
+  verSegundoStep(){
+    const content = this.formContent.nativeElement;
+    content.style.marginLeft = '-100%';
+    this.step = 2;
+  }
+
+  verTercerStep(){
+    const content = this.formContent.nativeElement;
+    content.style.marginLeft = '-200%';
+    this.step = 3;
+  }
+
+  verCuartoStep(){
+    const content = this.formContent.nativeElement;
+    content.style.marginLeft = '-300%';
+    this.step = 4;
   }
 
 }
